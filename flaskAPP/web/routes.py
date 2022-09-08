fc82aaae03add59aeed300e82a94316aa3e470d1
wpritom <refrigerator.rni.eed9@waltonplc.com>
2022-09-08 16:49:30 +0600

    internal api call building

--- flaskAPP/web/routes.py
+++ flaskAPP/web/routes.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, session, render_template, url_for, redirect
 from flask.views import MethodView
 from flaskAPP.extension import send_response
 
-web = Blueprint('web', __name__)
+import requests
+
+web = Blueprint('web', __name__, template_folder='templates')
 
 class Index(MethodView):
     init_every_request = False
@@ -18,13 +20,23 @@ class User(MethodView):
     init_every_request = False
 
     def get(self, value=None):
-        pass
-
+        # if 'username' in session:
+        #     return render_template("user.html")
+        data = requests.get(request.root_url + f'/api/{value}').json()
+        
+        return render_template("user.html")
     def post(self):
-        pass
+        # request.po
+        # print(dict(request.form))
+        # print(redirect(url_for('api.Index_post')))
+        
+        
+        return render_template("user.html")
 
 def register_view(app, url_prefix=None, method_view=None, name=None):
+    print(f'{url_prefix}/<value>')
     app.add_url_rule(f'{url_prefix}/<value>', view_func = method_view.as_view(f"{name}_get"))
     app.add_url_rule(f'{url_prefix}/', view_func = method_view.as_view(f"{name}_post"))
 
-register_view(web,'', Index, "Index")
+register_view(web, '', Index, "Index")
+register_view(web, 'user', User, "User")
